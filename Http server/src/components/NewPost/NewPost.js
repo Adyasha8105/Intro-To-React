691d1e1ee300121c4ee6044df3315f53f3ece8ca
adyasha <adyashamohanty8105@gmail.com>
2021-01-08 23:52:29 +0530

    Posts request, fetch posts

--- Http server/src/components/NewPost/NewPost.js
+++ Http server/src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -7,6 +7,19 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author,
+        };
+         axios.get('https://jsonplaceholder.typicode.com/posts',data)
+           .then(response => {
+               console.log(response);
+           });
+           
     }
 
     render () {
@@ -22,7 +35,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
--- Http server/src/containers/Blog/Blog.js
+++ Http server/src/containers/Blog/Blog.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts:[],
+        selectedPostId: null
+    }
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+         .then(response => {
+             const posts = response.data.slice(0,4); //used to post only 4
+             const updatedPosts = posts.map(post => {
+                 return{
+                     ...post,
+                     author:'Max'
+                 }
+             });
+             this.setState({posts: updatedPosts});
+             //console.log(response);
+         });
+    }
+
+    postSelectedHandler = (id) => {
+       this.setState({selectedPostId: id});
+    }
     render () {
+        const posts = this.state.posts.map(post => {
+               return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>;
+            });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
